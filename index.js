516f122e778d9147a5dd55f5f07a25f8801f1c0f
jbalata <jbalata@employeenavigator.com>
2020-02-12 14:19:12 -0500

    more koa testing

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const bodyParser = require('koa-bodyparser');
 const serve = require("koa-static");
 const mount = require("koa-mount");
 const http  = require('http')
+const routes = require('./src/routes')
 
 const app = new Koa()
   .use(cors())
   .use(logger())
-  .use(bodyParser());
+  .use(bodyParser())
 
 //These are the new change
 const static_pages = new Koa();
-static_pages.use(serve(__dirname + "build")); //serve the build directory
+static_pages.use(serve(__dirname + "/dist")); //serve the build directory
 
 app.use(mount("/", static_pages));
-app.use(require('koa-static')(__dirname + 'build'));
+app.use(require('koa-static')(__dirname + '/dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 app.listen(process.env.PORT, () => {
   console.log(`${'[SYS]'.rainbow} server started at port ${process.env.PORT}`);
 });
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = () => ({
   entry: [
     'babel-polyfill',
-    path.join(__dirname, 'src/index2.js'),
+    path.join(__dirname, './index.js'),
   ],
   output: {
     path: path.join(__dirname, 'dist'),
@@ -13,6 +13,10 @@ module.exports = () => ({
   devtool: '#eval-source-map',
   plugins: [
   ],
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  },
   module: {
     rules: [
       {

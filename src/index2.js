45880c4778dc9d0dbc4f9a1d3e26a5ee88a5b3f4
jbalata <jbalata@employeenavigator.com>
2020-02-21 13:51:43 -0500

    sighhh

--- src/index2.js
+++ src/index2.js
@@ -11,7 +11,7 @@ app.use(express.static('dist'));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(port);
